86f8b888ed5a7ce28b4c9d59c418290910ccc415
François Duchêne <francois.duchene@insyco.fr>
2025-07-19 12:06:40 +0200

    Fix Sepolia part in config file for direct deployment

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -14,6 +15,21 @@ module.exports = {
   networks: {
     hardhat: {
       chainId: 31337
-    }  
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/LbwylyvBxjSTGvUVy0tTS",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+      gasPrice: "auto"
+    }
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY
+    }
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD"
   }
 };
